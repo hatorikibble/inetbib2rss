ad8906856c748c1f8dd35249c4aeefffa658e98a
Peter Mayr <at.peter.mayr@posteo.de>
2016-08-14 14:28:48 +0200

    erste lauffähige version

--- cpanfile
+++ cpanfile
@@ -1,4 +1,6 @@
-requires 'HTML::TreeBuilder::XPath', '0.14';
+requires 'DateTime', '1.36';
+requires 'DateTime::Format::W3CDTF', '0.06';
+requires 'DateTime::Format::Strptime', '1.68';
 requires 'LWP::Simple', '6.15';
 requires 'XML::RSS', '1.59';
 
--- inetbib2rss.pl
+++ inetbib2rss.pl
@@ -5,34 +5,57 @@ use warnings;
 
 use Data::Dumper;
 
+use DateTime;
+use DateTime::Format::W3CDTF;
+use DateTime::Format::Strptime;
 use LWP::Simple;
-use HTML::TreeBuilder::XPath;
 use XML::RSS;
 
 my $InetBibArchiv = "http://www.inetbib.de/listenarchiv/";
 my $RssOutputFile = "feed.xml";
-my $Rss = XML::RSS->new(version=>'1.0');
+my $Rss           = XML::RSS->new( version => '1.0' );
 
 $Rss->channel(
-    title=>'InetBib Listenarchiv',
-    description=>'Die Mailingliste als RSS-Feed');
+    title       => 'InetBib Listenarchiv',
+    description => 'Die Mailingliste als RSS-Feed',
+    link        => $InetBibArchiv
+);
 
-print $Rss->as_string();
 my $content = get($InetBibArchiv);
+my $date    = undef;
+my $url     = undef;
+my $subject = undef;
+my $author  = undef;
 
-#print $content;
+my $DateParser = DateTime::Format::Strptime->new(
+    pattern  => '%d. %B %Y',
+    locale   => 'en',
+    on_error => 'croak',
+);
 
-my  $root = HTML::TreeBuilder->new_from_content($content);
+foreach my $l ( split( '\n', $content ) ) {
 
-#print $root->as_XML_indented;
-
-my $url = undef;
-my @messages = $root->findnodes('//body/ul/ul/li');
-
-foreach my $msg ($root->findnodes('//body/ul/ul/li')){
-    print $msg->findvalue('.');
-    $url = $msg->findvalue('./strong/a/@href');
-    print "\n";
+    #print $l;
+    if ( $l =~ /<LI><STRONG>(\d+\. \w+ \d{4})<\/STRONG><\/LI>/ ) {
+        $date = $DateParser->parse_datetime($1);
+    }
+    elsif ( $l =~
+/<LI><strong><a name="\d+" href="(.*?)">(.*?)<\/a><\/strong>, (.*?)<\/LI>/
+      )
+    {
+        $url     = $InetBibArchiv . $1;
+        $subject = $2;
+        $author  = $3;
+        $Rss->add_item(
+            title => $subject,
+            link  => $url,
+            dc    => {
+                creator => $author,
+                date    => DateTime::Format::W3CDTF->format_date($date)
+            }
+        );
+    }
 }
 
+print $Rss->as_string();
 
