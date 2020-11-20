047b6e5ff9d95d71ab56ea9aedf5291b165a3d99
hatorikibble <at.peter.mayr@posteo.de>
2020-11-20 16:50:50 +0100

    Logging

--- cpanfile
+++ cpanfile
@@ -2,5 +2,8 @@ requires 'DateTime', '1.36';
 requires 'DateTime::Format::W3CDTF', '0.06';
 requires 'DateTime::Format::Strptime', '1.68';
 requires 'HTTP::Tiny', '0.076';
+requires 'Log::Any';
+requires 'Log::Any::Adapter::Screen';
+requires 'Pod::Usage';
 requires 'XML::RSS', '1.59';
 
--- inetbib2rss.pl
+++ inetbib2rss.pl
@@ -8,18 +8,32 @@ use Data::Dumper;
 use DateTime;
 use DateTime::Format::W3CDTF;
 use DateTime::Format::Strptime;
+use Getopt::Long;
 use HTTP::Tiny;
+use Log::Any qw ($log);
+use Log::Any::Adapter;
+use Pod::Usage;
 use XML::RSS;
 
+$ENV{LOG_PREFIX} = "elapsed";
+Log::Any::Adapter->set('Screen',
+                       min_level => 'debug',
+                       stderr    => 0,
+
+                      );
+
+
+
 my $InetBibArchiv = "https://www.inetbib.de/listenarchiv/";
+
+my $help = 0;
+my $outfile = undef;
+
+sub createRssFile{
+
 my $Rss           = XML::RSS->new( version => '1.0' );
-
-$Rss->channel( title       => 'InetBib Listenarchiv',
-               description => 'Die Mailingliste als RSS-Feed',
-               link        => $InetBibArchiv
-);
-
 my $response         = undef;
+my $i = 0;
 my $content          = undef;
 my $date             = undef;
 my $url              = undef;
@@ -28,6 +42,7 @@ my $author           = undef;
 my $message_response = undef;
 my $message          = undef;
 my $desc             = undef;
+my $error = undef;
 
 my $DateParser =
     DateTime::Format::Strptime->new( pattern  => '%a, %e %b %Y %H:%M:%S %z',
@@ -37,15 +52,22 @@ my $DateParser =
 
 # Thu, 19 Nov 2020 17:42:48 +0100
 
+$log->debugf("Fetching '%s'",$InetBibArchiv);
 $response = HTTP::Tiny->new->get($InetBibArchiv);
 
 if ( $response->{success} ) {
+
+  $Rss->channel( title       => 'InetBib Listenarchiv',
+                 description => 'Die Mailingliste als RSS-Feed',
+                 link        => $InetBibArchiv
+  );
     $content = $response->{content};
     foreach my $l ( split( '\n', $content ) ) {
         if ( $l
             =~ /<LI><strong><a name="\d+" href="(.*?)">(.*?)<\/a><\/strong>, (.*?)<\/LI>/
             )
         {
+            $i++;
             $url              = $InetBibArchiv . $1;
             $subject          = $2;
             $author           = $3;
@@ -71,7 +93,7 @@ if ( $response->{success} ) {
             else {
                 $date = undef;
             }
-
+$log->debugf("Adding item %d: %s: \"%s\" (%s)",$i,$author,$subject,$date);
             $Rss->add_item(
                     title       => $subject,
                     link        => $url,
@@ -83,9 +105,65 @@ if ( $response->{success} ) {
             );
         } ## end if ( $l =~ ...)
     } ## end foreach my $l ( split( '\n'...))
+    $log->infof("Fetched %d items",$i);
 
-    print $Rss->as_string();
+
+    open (F,">",$outfile)
+     or die_with_error(sprintf("Kann '%s' nicht oeffnen: %s",$outfile,$!));
+    print F $Rss->as_string();
+    close(F) or die_with_error(sprintf("Kann '%s' nicht schliessen: %s",$outfile,$!));
+    $log->infof("created '%s' successfully",$outfile);
 } ## end if ( $response->{success...})
 else {
-    die "Konnte URL $InetBibArchiv nicht abrufen!";
+  $error = sprintf("ERROR: Konnte URL $InetBibArchiv nicht abrufen: %s (%s)",$InetBibArchiv,$response->{reason},$response->{status});
+  $log->error($error);
+    die $error;
 }
+
+}
+
+sub die_with_error{
+  my $error = shift;
+  $log->error("ERROR: ".$error);
+  die $error;
+}
+
+######
+# Main
+######
+
+$log->infof("Started at '%s'...", DateTime->now->strftime("%d.%m.%Y %H:%M:%S"));
+GetOptions('help|?' => \$help, "outfile=s" => \$outfile) or pod2usage(2);
+pod2usage(1) if $help;
+
+die_with_error("Kein Parameter 'outfile' uebergeben!") unless (defined($outfile));
+
+createRssFile();
+$log->infof("Ended at '%s'...", DateTime->now->strftime("%d.%m.%Y %H:%M:%S"));
+__END__
+
+=head1 NAME
+
+inetbib2rss.pl - RSS Feed aus dem InetBib Mailarchiv erzeugen
+
+=head1 SYNOPSIS
+
+./inetbib2rss.pl -o=/var/www/rss/inetbib.xml
+
+./inetbib2rss.pl --outfile=/var/www/rss/inetbib.xml
+
+=head1 OPTIONS
+
+=over 8
+
+=item * (-o|--outfile)
+
+Name der Datei, in die der RSS-Feed geschrieben werden soll
+
+=item * (-h|--help|-?)
+
+diese Hilfe anzeigen
+
+=back
+
+=cut
