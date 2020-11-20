aba8b12431799ac54b140e95b0a36359ae3bc0c1
hatorikibble <at.peter.mayr@posteo.de>
2020-11-20 10:14:32 +0100

    Datumsformat hat sich geändert, Perltidy, Abbruch wenn URL nicht verfuegbar ist

--- inetbib2rss.pl
+++ inetbib2rss.pl
@@ -14,13 +14,12 @@ use XML::RSS;
 my $InetBibArchiv = "http://www.inetbib.de/listenarchiv/";
 my $Rss           = XML::RSS->new( version => '1.0' );
 
-$Rss->channel(
-    title       => 'InetBib Listenarchiv',
-    description => 'Die Mailingliste als RSS-Feed',
-    link        => $InetBibArchiv
+$Rss->channel( title       => 'InetBib Listenarchiv',
+               description => 'Die Mailingliste als RSS-Feed',
+               link        => $InetBibArchiv
 );
 
-my $content = get($InetBibArchiv);
+my $content = undef;
 my $date    = undef;
 my $url     = undef;
 my $subject = undef;
@@ -28,44 +27,55 @@ my $author  = undef;
 my $message = undef;
 my $desc    = undef;
 
-my $DateParser = DateTime::Format::Strptime->new(
-    pattern  => '%a, %e %b %Y %H:%M:%S %z (%Z)',
-    locale   => 'en',
-    on_error => 'croak',
-);
+my $DateParser =
+    DateTime::Format::Strptime->new( pattern  => '%a, %e %b %Y %H:%M:%S %z',
+                                     locale   => 'en',
+                                     on_error => 'croak',
+    );
 
-foreach my $l ( split( '\n', $content ) ) {
-    if ( $l =~
-/<LI><strong><a name="\d+" href="(.*?)">(.*?)<\/a><\/strong>, (.*?)<\/LI>/
-      )
-    {
-        $url     = $InetBibArchiv . $1;
-        $subject = $2;
-        $author  = $3;
-        $message = get($url);
-        if ( $message =~ /Body-of-Message-->(.*?)<!--X-Body-of-Message/s ) {
-            $desc = $1;
-        }
-        else {
-            $desc = undef;
-        }
-        if ( $message =~ /X-Date: (.+?) --/ ) {
-            $date = $DateParser->parse_datetime($1);
-        }
-        else {
-            $date = undef;
-        }
-        $Rss->add_item(
-            title       => $subject,
-            link        => $url,
-            description => $desc,
-            dc          => {
-                creator => $author,
-                date    => DateTime::Format::W3CDTF->format_datetime($date)
+# Thu, 19 Nov 2020 17:42:48 +0100
+
+if ( $content = get($InetBibArchiv) ) {
+
+    foreach my $l ( split( '\n', $content ) ) {
+        if ( $l
+            =~ /<LI><strong><a name="\d+" href="(.*?)">(.*?)<\/a><\/strong>, (.*?)<\/LI>/
+            )
+        {
+            $url     = $InetBibArchiv . $1;
+            $subject = $2;
+            $author  = $3;
+            $message = get($url) or die "Konnte URL $url nicht abrufen!";
+            if ( $message =~ /Body-of-Message-->(.*?)<!--X-Body-of-Message/s ) {
+                $desc = $1;
             }
-        );
-    }
+            else {
+                $desc = undef;
+            }
+
+            # <!--X-Date: Thu, 19 Nov 2020 17:42:48 +0100 -->
+            if ( $message =~ /X-Date: (.+?) --/ ) {
+
+                $date = $DateParser->parse_datetime($1);
+            }
+            else {
+                $date = undef;
+            }
+
+            $Rss->add_item(
+                    title       => $subject,
+                    link        => $url,
+                    description => $desc,
+                    dc          => {
+                        creator => $author,
+                        date => DateTime::Format::W3CDTF->format_datetime($date)
+                    }
+            );
+        } ## end if ( $l =~ ...)
+    } ## end foreach my $l ( split( '\n'...))
+
+    print $Rss->as_string();
+} ## end if ( $content = get($InetBibArchiv...))
+else {
+    die "Konnte URL $InetBibArchiv nicht abrufen!";
 }
-
-print $Rss->as_string();
-
