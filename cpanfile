e285e7c254abc407b4c7a196c0a8327bc9513b17
hatorikibble <at.peter.mayr@posteo.de>
2020-11-20 11:51:18 +0100

    HTTP::Tiny und https verwenden

--- cpanfile
+++ cpanfile
@@ -1,6 +1,6 @@
 requires 'DateTime', '1.36';
 requires 'DateTime::Format::W3CDTF', '0.06';
 requires 'DateTime::Format::Strptime', '1.68';
-requires 'LWP::Simple', '6.15';
+requires 'HTTP::Tiny', '0.076';
 requires 'XML::RSS', '1.59';
 
--- inetbib2rss.pl
+++ inetbib2rss.pl
@@ -8,10 +8,10 @@ use Data::Dumper;
 use DateTime;
 use DateTime::Format::W3CDTF;
 use DateTime::Format::Strptime;
-use LWP::Simple;
+use HTTP::Tiny;
 use XML::RSS;
 
-my $InetBibArchiv = "http://www.inetbib.de/listenarchiv/";
+my $InetBibArchiv = "https://www.inetbib.de/listenarchiv/";
 my $Rss           = XML::RSS->new( version => '1.0' );
 
 $Rss->channel( title       => 'InetBib Listenarchiv',
@@ -19,13 +19,15 @@ $Rss->channel( title       => 'InetBib Listenarchiv',
                link        => $InetBibArchiv
 );
 
-my $content = undef;
-my $date    = undef;
-my $url     = undef;
-my $subject = undef;
-my $author  = undef;
-my $message = undef;
-my $desc    = undef;
+my $response         = undef;
+my $content          = undef;
+my $date             = undef;
+my $url              = undef;
+my $subject          = undef;
+my $author           = undef;
+my $message_response = undef;
+my $message          = undef;
+my $desc             = undef;
 
 my $DateParser =
     DateTime::Format::Strptime->new( pattern  => '%a, %e %b %Y %H:%M:%S %z',
@@ -35,17 +37,25 @@ my $DateParser =
 
 # Thu, 19 Nov 2020 17:42:48 +0100
 
-if ( $content = get($InetBibArchiv) ) {
+$response = HTTP::Tiny->new->get($InetBibArchiv);
 
+if ( $response->{success} ) {
+    $content = $response->{content};
     foreach my $l ( split( '\n', $content ) ) {
         if ( $l
             =~ /<LI><strong><a name="\d+" href="(.*?)">(.*?)<\/a><\/strong>, (.*?)<\/LI>/
             )
         {
-            $url     = $InetBibArchiv . $1;
-            $subject = $2;
-            $author  = $3;
-            $message = get($url) or die "Konnte URL $url nicht abrufen!";
+            $url              = $InetBibArchiv . $1;
+            $subject          = $2;
+            $author           = $3;
+            $message_response = HTTP::Tiny->new->get($url);
+            if ( $message_response->{success} ) {
+                $message = $message_response->{content};
+            }
+            else {
+                die "Konnte URL $url nicht abrufen!";
+            }
             if ( $message =~ /Body-of-Message-->(.*?)<!--X-Body-of-Message/s ) {
                 $desc = $1;
             }
@@ -75,7 +85,7 @@ if ( $content = get($InetBibArchiv) ) {
     } ## end foreach my $l ( split( '\n'...))
 
     print $Rss->as_string();
-} ## end if ( $content = get($InetBibArchiv...))
+} ## end if ( $response->{success...})
 else {
     die "Konnte URL $InetBibArchiv nicht abrufen!";
 }
