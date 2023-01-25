2833725da96c62f1ec76b30ff69ae18dad338ce9
Peter Mayr <at.peter.mayr@posteo.de>
2023-01-25 12:04:35 +0100

    bei Netzwerkprobleme Request wiederholen

--- inetbib2rss.pl
+++ inetbib2rss.pl
@@ -37,31 +37,46 @@ sub createRssFile {
     my $message          = undef;
     my $desc             = undef;
     my $error            = undef;
+    my $retry_sec_range  = 7;
+    my $retry_sec        = undef;
 
-    my $DateParser =
-        DateTime::Format::Strptime->new( pattern  => '%a, %e %b %Y %H:%M:%S %z',
-                                         locale   => 'en',
-                                         on_error => 'croak',
-        );
+    my $DateParser = DateTime::Format::Strptime->new(
+        pattern  => '%a, %e %b %Y %H:%M:%S %z',
+        locale   => 'en',
+        on_error => 'croak',
+    );
 
     # Thu, 19 Nov 2020 17:42:48 +0100
 
     $log->debugf( "Fetching %d items from '%s'", $limit, $InetBibArchiv );
-    $response = HTTP::Tiny->new->get($InetBibArchiv);
 
+    for ( my $i = 1 ; $i < 10 ; $i++ ) {
+        $log->debugf( "%d. request...", $i );
+        $response = HTTP::Tiny->new->get($InetBibArchiv);
+
+        last if ( $response->{success} );
+
+        $retry_sec = int( rand($retry_sec_range) );
+        $log->errorf(
+            "ERROR: Konnte URL nicht abrufen: %s: %s, warte %d Sekunden",
+            $response->{status}, $response->{reason}, $retry_sec );
+        sleep($retry_sec);
+
+    }
     if ( $response->{success} ) {
 
-        $Rss->channel( title       => 'InetBib Listenarchiv',
-                       description =>
-                           sprintf( 'Die Mailingliste als RSS-Feed (Stand: %s)',
-                                 DateTime->now(time_zone=>'Europe/Berlin')->strftime("%d.%m.%Y %H:%M:%S") ),
-                       link => $InetBibArchiv
+        $Rss->channel(
+            title       => 'InetBib Listenarchiv',
+            description => sprintf( 'Die Mailingliste als RSS-Feed (Stand: %s)',
+                DateTime->now( time_zone => 'Europe/Berlin' )
+                  ->strftime("%d.%m.%Y %H:%M:%S") ),
+            link => $InetBibArchiv
         );
         $content = $response->{content};
         foreach my $l ( split( '\n', $content ) ) {
-            if ( $l
-                =~ /<LI><strong><a name="\d+" href="(.*?)">(.*?)<\/a><\/strong>, (.*?)<\/LI>/
-                )
+            if ( $l =~
+/<LI><strong><a name="\d+" href="(.*?)">(.*?)<\/a><\/strong>, (.*?)<\/LI>/
+              )
             {
                 next if ( $i >= $limit );    # Limit erreicht?
                 $i++;
@@ -75,8 +90,8 @@ sub createRssFile {
                 else {
                     die "Konnte URL $url nicht abrufen!";
                 }
-                if ( $message
-                     =~ /Body-of-Message-->(.*?)<!--X-Body-of-Message/s )
+                if ( $message =~
+                    /Body-of-Message-->(.*?)<!--X-Body-of-Message/s )
                 {
                     $desc = $1;
                 }
@@ -93,7 +108,7 @@ sub createRssFile {
                     $date = undef;
                 }
                 $log->debugf( "Adding item %d: %s: \"%s\" (%s)",
-                              $i, $author, $subject, $date );
+                    $i, $author, $subject, $date );
                 $Rss->add_item(
                     title       => $subject,
                     link        => $url,
@@ -108,21 +123,21 @@ sub createRssFile {
         $log->infof( "Fetched %d items", $i );
 
         open( F, ">", $outfile )
-            or die_with_error(
-                       sprintf( "Kann '%s' nicht oeffnen: %s", $outfile, $! ) );
+          or die_with_error(
+            sprintf( "Kann '%s' nicht oeffnen: %s", $outfile, $! ) );
         print F $Rss->as_string();
         close(F)
-            or die_with_error(
-                    sprintf( "Kann '%s' nicht schliessen: %s", $outfile, $! ) );
+          or die_with_error(
+            sprintf( "Kann '%s' nicht schliessen: %s", $outfile, $! ) );
         $log->infof( "created '%s' successfully", $outfile );
     } ## end if ( $response->{success...})
     else {
-        $error
-            = sprintf(
-                     "ERROR: Konnte URL $InetBibArchiv nicht abrufen: %s (%s: %s)",
-                     $InetBibArchiv, $response->{status}, $response->{reason} );
-        $log->error($error);
-        die $error;
+        die_with_error(
+            sprintf(
+                "ERROR: Konnte URL %s nicht abrufen: %s: %s",
+                $InetBibArchiv, $response->{status}, $response->{reason}
+            )
+        );
     }
 
 } ## end sub createRssFile
@@ -137,18 +152,26 @@ sub die_with_error {
 # Main
 ######
 
-$log->infof( "Started at '%s'...",
-             DateTime->now(time_zone=>'Europe/Berlin')->strftime("%d.%m.%Y %H:%M:%S") );
-GetOptions( 'help|?' => \$help, "outfile=s" => \$outfile, "limit=i" => \$limit )
-    or pod2usage(2);
+$log->infof(
+    "Started at '%s'...",
+    DateTime->now( time_zone => 'Europe/Berlin' )->strftime("%d.%m.%Y %H:%M:%S")
+);
+GetOptions(
+    'help|?'    => \$help,
+    "outfile=s" => \$outfile,
+    "limit=i"   => \$limit
+) or pod2usage(2);
 pod2usage(1) if $help;
 
 die_with_error("Kein Parameter 'outfile' uebergeben!")
-    unless ( defined($outfile) );
+  unless ( defined($outfile) );
 $limit = 50 unless ( defined($limit) );
 
 createRssFile();
-$log->infof( "Ended at '%s'...", DateTime->now(time_zone=>'Europe/Berlin')->strftime("%d.%m.%Y %H:%M:%S") );
+$log->infof(
+    "Ended at '%s'...",
+    DateTime->now( time_zone => 'Europe/Berlin' )->strftime("%d.%m.%Y %H:%M:%S")
+);
 __END__
 
 =encoding utf8
